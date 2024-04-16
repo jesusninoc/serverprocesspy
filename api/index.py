71a762f9558ca7e71845dbe14325bffbf36f1f88
Jesús <16318863+jesusninoc@users.noreply.github.com>
2024-04-16 07:16:56 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
-import psutil
 
 app = FastAPI()
 
-@app.get("/api/procesos")
-async def obtener_procesos():
-    # Obtener una lista de todos los procesos del sistema
-    lista_procesos = []
-    for proceso in psutil.process_iter():
-        try:
-            info = proceso.as_dict(attrs=['pid', 'name', 'username'])
-            lista_procesos.append(info)
-        except psutil.NoSuchProcess:
-            pass
-    
-    return {"procesos": lista_procesos}
+@app.post("/api/")
+async def hola():
+    return {"message": "Hola, Mundo"}
